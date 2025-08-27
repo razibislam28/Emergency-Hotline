b6ea3e7ec4aea38a24587ff40980796ed3ab5c4b
razibislam28 <razibislam28@gmail.com>
2025-08-27 12:05:29 +0600

    added the call button

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ const likeButtons = document.querySelectorAll(".likeBtn");
 likeButtons.forEach((button) => {
   button.addEventListener("click", updateHearts);
 });
-
+// ----------------------------
+// ----------------------------
 // COPY BUTTON
+// ----------------------------
+// ----------------------------
 function copyNumber() {
   // Get the card body of this button
   const cardBody = this.closest(".card-body");
@@ -44,4 +47,41 @@ function copyNumber() {
 // Attach to all copy buttons
 document.querySelectorAll(".copyBtn").forEach((btn) => {
   btn.addEventListener("click", copyNumber);
+});
+
+// ----------------------------
+// ----------------------------
+// CALL BUTTON
+// ----------------------------
+// ----------------------------
+function makeCall() {
+  // Get the card body of this button
+  const cardBody = this.closest(".card-body");
+
+  // Get the name and displayed number
+  const serviceName = cardBody.querySelector("h2").textContent;
+  const number = cardBody.querySelector("p.text-3xl").textContent;
+
+  // Check if enough coins
+  if (coinCount < 20) {
+    alert("Not enough coins to make a call!");
+    return;
+  }
+
+  coinCount -= 20;
+  coinCountEl.textContent = coinCount;
+
+  // Show alert with service name and the displayed numbr
+  alert(`Calling ${serviceName} (${number})`);
+
+  // Add to call history
+  const time = new Date().toLocaleTimeString();
+  const li = document.createElement("li");
+  li.textContent = `${serviceName} - ${number} at ${time}`;
+  historyList.appendChild(li);
+}
+
+// Attach to all call buttons
+document.querySelectorAll(".callBtn").forEach((btn) => {
+  btn.addEventListener("click", makeCall);
 });
